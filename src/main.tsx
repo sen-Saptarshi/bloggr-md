d7c70ee79100b87567ec6701ac0d2bafa3ddf38f
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2025-06-17 10:50:14 +0530

    init2

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "./components/theme-provider.tsx";
 import { HashRouter } from "react-router";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
