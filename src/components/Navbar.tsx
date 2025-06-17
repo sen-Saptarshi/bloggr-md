78b5955a70eaecbc3f9a906761380ccb9dd0a496
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2025-06-17 12:18:04 +0530

    fix:ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { BookAIcon } from "lucide-react";
 export default function Navbar() {
   return (
     <>
-      <div className="flex items-center justify-between px-2 py-2 mx-auto max-w-200 sticky top-0 z-50 bg-background/50 backdrop-blur-md">
+      <div className="flex items-center justify-between px-2 py-2 mx-auto max-w-200 md:max-w-4xl sticky top-0 z-50 bg-background/50 backdrop-blur-md">
         <Link to="/">
           <div className="text-3xl font-bold font-mono flex items-center">
             <BookAIcon className="w-10 h-10" />
