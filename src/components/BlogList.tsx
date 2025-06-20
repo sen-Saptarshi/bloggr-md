d7f9837d7cd5925c3fd919e87fc99a6644966670
sen-Saptarshi <113275417+sen-Saptarshi@users.noreply.github.com>
2025-06-20 17:40:35 +0530

    feat: latest tag

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -8,25 +8,37 @@ interface PostListProps {
 export function PostList({ posts }: PostListProps) {
   return (
     <ul className="flex flex-col gap-4">
-      {posts.map((post) => (
-        <li key={post.path} className="flex flex-col border-b-2 pb-2">
-          <Link to={`/blog/${post.path}`}>
-            <h2 className="text-2xl cursor-pointer hover:underline">
-              {post.attributes.title}
-            </h2>
-          </Link>
-          <p className="text-foreground/60 selection:bg-amber-500">
-            {post.attributes.description}
-          </p>
-          <div className="mt-2">
-            <p className="text-sm">
-              {post.attributes.date} ·{" "}
-              <span className="font-semibold">{post.attributes.author}</span>
+      {posts.map((post) => {
+        const postDate = new Date(post.attributes.date);
+        const isNew =
+          (new Date().getTime() - postDate.getTime()) / (1000 * 60 * 60 * 24) <
+          7;
+
+        return (
+          <li key={post.path} className="flex flex-col border-b-2 pb-2">
+            <Link to={`/blog/${post.path}`} className="flex items-center">
+              <h2 className="text-2xl cursor-pointer hover:underline">
+                {post.attributes.title}
+              </h2>
+              {isNew && (
+                <div className="ml-2 bg-primary text-secondary text-xs rounded-full px-2">
+                  Latest
+                </div>
+              )}
+            </Link>
+            <p className="text-foreground/60 selection:bg-amber-500">
+              {post.attributes.description}
             </p>
-            <TagList tags={post.attributes.tags} />
-          </div>
-        </li>
-      ))}
+            <div className="mt-2">
+              <p className="text-sm">
+                {post.attributes.date} ·{" "}
+                <span className="font-semibold">{post.attributes.author}</span>
+              </p>
+              <TagList tags={post.attributes.tags} />
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
